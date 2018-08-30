3929654d972dd902ae37314058b85623025298f3
Chovin Carlson <Chovin@users.noreply.github.com>
2018-08-30 00:37:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board()
   board = Array.new 9, " "
-  rows = (0..i).each {|r| " ${board[r*3]} | ${board[r*3+1]} | ${board[r*3+2]} \n"}
+  cells = board.each {|i| " #{i} "}
+  rows = (0..2).each((0..2).each())
   rows.join ""
 end
