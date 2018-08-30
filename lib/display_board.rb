2d03fc065b623bc6a6ccaa148863f305ba12f1d4
Chovin Carlson <Chovin@users.noreply.github.com>
2018-08-30 00:53:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,6 @@
 def display_board()
   board = Array.new 9, " "
   cells = board.each {|i| " #{i} "}
-  rows = (0..9).step(3).each {|r| cells[r * 3, r * 3]}
+  rows = (0...9).step(3).each {|r| cells[r * 3, r * 3]}
   rows.join ""
 end
